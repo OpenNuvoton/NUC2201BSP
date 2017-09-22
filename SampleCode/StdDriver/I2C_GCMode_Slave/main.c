aae56f3532f65be859c5893efa9aba1010cd3327
cytseng0 <cytseng0@nuvoton.com>
2017-09-22 19:47:44 +0800

    1. Modify IAR project settings for IAR v6.5 and flash loader 2. Modify main.c comment 3. Update sample code .bin
    Change-Id: I86ea26a99e694d0edce0f0c6a0e7d9570cd99ef9

--- SampleCode/StdDriver/I2C_GCMode_Slave/main.c
+++ SampleCode/StdDriver/I2C_GCMode_Slave/main.c
@@ -223,7 +223,7 @@ int32_t main(void)
 
     printf("\n");
     printf("+---------------------------------------------------------------------+\n");
-    printf("| NUC2201 I2C Driver Sample Code (Slave) for access Slave (GC Mode)|\n");
+    printf("| NUC2201 I2C Driver Sample Code (Slave) for access Slave (GC Mode)   |\n");
     printf("|                                                                     |\n");
     printf("| I2C Master (I2C0) <---> I2C Slave(I2C0)(Address: 0x00)              |\n");
     printf("+---------------------------------------------------------------------+\n");
