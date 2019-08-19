5967c328b4691b47adc0e8fcea0892c81ca7fb80
yjchen5 <yjchen5@nuvoton.com>
2019-08-19 17:28:07 +0800

    Add USBD_MassStorage_SDCard sample code.
    Change-Id: Ic11766d43df6b50e677c757676e2aa4b59f16d93

---
+++ SampleCode/StdDriver/USBD_MassStorage_SDCard/main.c
@@ -0,0 +1,173 @@
+/******************************************************************************
+ * @file     main.c
+ * @brief
+ *           Use SD card as storage to implement a USB Mass-Storage device.
+ * @note
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "massstorage.h"
+#include "SDCARD.h"
+
+#define CRYSTAL_LESS        1
+#define TRIM_INIT           (GCR_BASE+0x118)
+
+/*--------------------------------------------------------------------------*/
+
+void EnableCLKO(uint32_t u32ClkSrc, uint32_t u32ClkDiv)
+{
+    /* CLKO = clock source / 2^(u32ClkDiv + 1) */
+    CLK->FRQDIV = CLK_FRQDIV_DIVIDER_EN_Msk | u32ClkDiv;
+
+    /* Enable CLKO clock source */
+    CLK->APBCLK |= CLK_APBCLK_FDIV_EN_Msk;
+
+    /* Select CLKO clock source */
+    CLK->CLKSEL2 = (CLK->CLKSEL2 & (~CLK_CLKSEL2_FRQDIV_S_Msk)) | u32ClkSrc;
+}
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 22.1184 MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCON_OSC22M_EN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_CLKSTATUS_OSC22M_STB_Msk);
+
+    /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLK_S_HIRC, CLK_CLKDIV_HCLK(1));
+
+#if (!CRYSTAL_LESS)
+    /* Enable external XTAL 12 MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCON_XTL12M_EN_Msk);
+
+    /* Waiting for external XTAL clock ready */
+    CLK_WaitClockReady(CLK_CLKSTATUS_XTL12M_STB_Msk);
+
+    /* Set core clock */
+    CLK_SetCoreClock(72000000);
+
+    /* Set Flash Access Delay */
+    FMC->FATCON |= (FMC_FATCON_FOMSEL0_Msk | FMC_FATCON_FOMSEL1_Msk);
+
+    /* Select module clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART_S_HIRC, CLK_CLKDIV_UART(1));
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USB_S_PLL, CLK_CLKDIV_USB(3));
+#else
+    /* Enable Internal RC 48MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCON_OSC48M_EN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_CLKSTATUS_OSC48M_STB_Msk);
+
+    /* Set core clock */
+    CLK_SetCoreClock(72000000);
+
+    /* Set Flash Access Delay */
+    FMC->FATCON |= (FMC_FATCON_FOMSEL0_Msk | FMC_FATCON_FOMSEL1_Msk);
+
+    /* Select module clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART_S_HIRC, CLK_CLKDIV_UART(1));
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USB_S_RC48M, CLK_CLKDIV_USB(1));
+#endif
+
+    /* Enable module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(USBD_MODULE);
+    CLK_EnableModuleClock(SPI1_MODULE);
+
+    CLK_SetModuleClock(SPI1_MODULE, CLK_CLKSEL1_SPI1_S_HCLK, MODULE_NoMsk);
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD, and Clock Output */
+    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk | SYS_GPB_MFP_PB8_Msk);
+    SYS->ALT_MFP &= ~SYS_ALT_MFP_PB8_Msk;
+    SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD | SYS_GPB_MFP_PB8_CLKO);
+    SYS->ALT_MFP |=  SYS_ALT_MFP_PB8_CLKO;
+
+    /* Set SPI1 multi-function pins */
+    SYS->GPC_MFP &= ~(SYS_GPC_MFP_PC9_Msk | SYS_GPC_MFP_PC10_Msk | SYS_GPC_MFP_PC11_Msk);
+    SYS->GPC_MFP |= (SYS_GPC_MFP_PC9_SPI1_CLK | SYS_GPC_MFP_PC10_SPI1_MISO0 | SYS_GPC_MFP_PC11_SPI1_MOSI0);
+    SYS->ALT_MFP &= ~(SYS_ALT_MFP_PC9_Msk | SYS_ALT_MFP_PC10_Msk | SYS_ALT_MFP_PC11_Msk);
+    SYS->ALT_MFP |= (SYS_ALT_MFP_PC9_SPI1_CLK | SYS_ALT_MFP_PC10_SPI1_MISO0 | SYS_ALT_MFP_PC11_SPI1_MOSI0);
+    GPIO_SetMode(PC, BIT15, GPIO_PMD_OUTPUT);
+    GPIO_SetMode(PE, BIT5, GPIO_PMD_OUTPUT);
+
+    /* Enable CLKO (PB.8) for monitor HCLK. CLKO = HCLK/8 Hz*/
+    EnableCLKO((2 << CLK_CLKSEL2_FRQDIV_S_Pos), 2);
+}
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+    uint32_t u32TrimInit;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    SYS_Init();
+
+    /* Configure UART0: 115200, 8-bit word, no parity bit, 1 stop bit. */
+    UART_Open(UART0, 115200);
+
+    printf("+-------------------------------------------------------+\n");
+    printf("|          NuMicro USB MassStorage Sample Code          |\n");
+    printf("+-------------------------------------------------------+\n");
+
+    SDCARD_Open();
+
+    USBD_Open(&gsInfo, MSC_ClassRequest, NULL);
+
+    USBD_SetConfigCallback(MSC_SetConfig);
+
+    /* Endpoint configuration */
+    MSC_Init();
+    USBD_Start();
+    NVIC_EnableIRQ(USBD_IRQn);
+
+#if CRYSTAL_LESS
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+#endif
+
+    while(1)
+    {
+#if CRYSTAL_LESS
+        /* Start USB trim if it is not enabled. */
+        if((SYS->HIRCTCTL & SYS_HIRCTCTL_FREQSEL_Msk) != 1)
+        {
+            /* Re-enable crystal-less */
+            SYS->HIRCTCTL = 0x201 | (31 << SYS_HIRCTCTL_BOUNDARY_Pos);
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->HIRCTSTS & (SYS_HIRCTSTS_CLKERIF_Msk | SYS_HIRCTSTS_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            if((u32TrimInit < 0x1E6) || (u32TrimInit > 0x253))
+                /* Re-enable crystal-less */
+                SYS->HIRCTCTL = 0x201 | (1 << SYS_HIRCTCTL_BOUNDARY_Pos);
+
+            /* Clear error flags */
+            SYS->HIRCTSTS = SYS_HIRCTSTS_CLKERIF_Msk | SYS_HIRCTSTS_TFAILIF_Msk;
+        }
+#endif
+
+        MSC_ProcessCmd();
+    }
+}
+
+/*** (C) COPYRIGHT 2019 Nuvoton Technology Corp. ***/
