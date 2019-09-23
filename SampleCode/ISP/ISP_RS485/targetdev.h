fc41ca9f2dbbe7e2d0070055e8de063587f95e5f
“ycchen5” <YCCHEN5@nuvoton.com>
2019-09-23 14:50:31 +0800

    Add ISP_UART and ISP_RS485 sample code.
    Change-Id: I812ea6a706da8c3f0e62ad923de9d0c084de6925

---
+++ SampleCode/ISP/ISP_RS485/targetdev.h
@@ -0,0 +1,20 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x31
+ *
+ * @note
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "NuMicro.h"
+#include "uart_transfer.h"
+#include "isp_user.h"
+
+
+extern __align(4) uint8_t response_buff[64];
+
+
+/* rename for uart_transfer.c */
+#define UART_N						UART1
+#define UART_N_IRQHandler		    UART1_IRQHandler
+#define UART_N_IRQn					UART1_IRQn
